ccf2feafe94a38682696a3772bef86ff8c9a794f
Ryan Toner <ryan.toner@student.fairfield.edu>
2021-04-09 00:05:49 -0400

    2 line perm string, refactored static methods

--- PathfindingTutorial/Data Structures/Graph.cs
+++ PathfindingTutorial/Data Structures/Graph.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace PathfindingTutorial.Data_Structures
 {
-    public class Graph<T> : IGraph<T>
+    public partial class Graph<T> : IGraph<T>
     {
         protected readonly List<IGraphNode<T>> graphStructure = new();
 
@@ -56,29 +56,6 @@ namespace PathfindingTutorial.Data_Structures
 
             return adjacencyMatrix;
         }
-
-        public static Graph<int> GenerateGraphForAdjacencyMatrix(int[,] adjacencyMatrix, bool undirected = false)
-        {
-            int length = adjacencyMatrix.GetLength(0);
-
-            var G = new Graph<int>();
-
-            for (int i = 0; i < length; i++)
-                G.AddNode(new GraphNode<int>(i));
-
-            for (int i = 0; i < length; i++)
-                for (int j = 0; j < length; j++)
-                    if (adjacencyMatrix[i, j] != 0)
-                    {
-                        G.TotalEdges++;
-                        if (undirected)
-                            IGraphNode<int>.AddMutualNeighbor(G.graphStructure[i], G.graphStructure[j]);
-                        else
-                            G.graphStructure[i].AddNeighbor(G.graphStructure[j]);
-                    }
-
-            return G;
-        }
         #endregion
 
         #region Degree Sequence
@@ -86,14 +63,15 @@ namespace PathfindingTutorial.Data_Structures
         /// Obtain a sorted degree sequence for the graph from greatest to least
         /// </summary>
         /// <returns></returns>
-        public List<int> GetDegreeSequence()
+        public List<int> GetDegreeSequence(bool sort=true)
         {
             var degreeSequence = new List<int>(graphStructure.Count);
 
             foreach (var node in graphStructure)
                 degreeSequence.Add(node.GetDegree());
 
-            degreeSequence.Sort((x, y) => y.CompareTo(x));
+            if(sort)
+                degreeSequence.Sort((x, y) => y.CompareTo(x));
 
             return degreeSequence;
         }
@@ -107,71 +85,6 @@ namespace PathfindingTutorial.Data_Structures
             {
                 this.Vertex = Vertex;
                 this.DegreeValue = DegreeValue;
-            }
-        }
-
-        /// <summary>
-        /// Creates a complete graph
-        /// </summary>
-        /// <param name="n"></param>
-        /// <returns></returns>
-        public static Graph<int> GenerateCompleteGraph(int n)
-        {
-            int[] degreeSequence = new int[n];
-            for (int i = 0; i < n; i++)
-                degreeSequence[i] = n-1;
-            return GenerateGraphForDegreeSequence(degreeSequence);
-        }
-
-        /// <summary>
-        /// Generate a graph that matches the given degree sequence. Returns null if unable to actualize.
-        /// </summary>
-        /// <param name="degreeSequence"></param>
-        /// <returns></returns>
-        public static Graph<int> GenerateGraphForDegreeSequence(int[] degreeSequence)
-        {
-            var degreeSequenceList = new List<DegreeSequenceData>(degreeSequence.Length);
-
-            var G = new Graph<int>();
-            for (int i = 0; i < degreeSequence.Length; i++)
-            {
-                var node = new GraphNode<int>(i);
-                degreeSequenceList.Add(new DegreeSequenceData(node, degreeSequence[i]));
-                G.AddNode(node);
-            }
-
-            while (true)
-            {
-                //sort the degree sequence from greatest to least degree value
-                //if multiple have the same degree value, pick the smaller vertex
-                degreeSequenceList.Sort((x, y) => x == y ? x.Vertex.GetValue().CompareTo(y.Vertex.GetValue()) : y.DegreeValue.CompareTo(x.DegreeValue));
-
-                var maxDegreeData = degreeSequenceList[0];
-
-                //get the maximum degree vertex
-                int maxDegree = maxDegreeData.DegreeValue;
-
-                //if the maximum degree vertex is 0, we can terminate the algorithm
-                if (maxDegree == 0)
-                    return G;
-
-                //assign its degree to 0, as we will assign edges to use up all its degrees
-                degreeSequenceList[0].DegreeValue = 0;
-
-                //not enough nodes remain to add neighbors to
-                if(maxDegree >= degreeSequence.Length)
-                    return null;
-
-                //assign a connected to the next maxDegree vertices, and decrement each degree value by 1
-                for (int i = 1; i <= maxDegree; i++)
-                {
-                    degreeSequenceList[i].DegreeValue--;
-                    //the neighbor node cannot be assigned, and therefore this degree sequence is not possible
-                    if (degreeSequenceList[i].DegreeValue < 0)
-                        return null;
-                    //assign the vertex to each neighbor
-                    IGraphNode<int>.AddMutualNeighbor(maxDegreeData.Vertex, degreeSequenceList[i].Vertex);
-                }
             }
         }
         #endregion
@@ -240,6 +153,11 @@ namespace PathfindingTutorial.Data_Structures
         public Graph(params IGraphNode<T>[] nodes)
         {
             graphStructure = new List<IGraphNode<T>>(nodes);
+            TotalVertices = nodes.Length;
+            int degreeSum = 0;
+            foreach (var node in nodes)
+                degreeSum += node.GetDegree();
+            TotalEdges = degreeSum / 2;
         }
 
         public Graph(IGraphNode<T>[,] nodes)
@@ -735,251 +653,6 @@ namespace PathfindingTutorial.Data_Structures
             }
         }
 
-        // Generating permutation using Heap Algorithm
-        private static void heapPermutations(int[] a, int size, int n, List<int[]> permutations)
-        {
-            var stk = new Stack<(int[] a, int size, int n)>();
-
-            stk.Add((a, size, n));
-
-            while (stk.Count > 0)
-            {
-                var (a1, size1, n1) = stk.Pop();
-
-                if (size1 == 1)
-                {
-                    permutations.Add(a1);
-                    continue;
-                }
-
-                for (int i = 0; i < size1; i++)
-                {
-                    stk.Add((a1, size1 - 1, n1));
-
-                    a1 = (int[])a1.Clone();
-
-                    // if size is odd, swap 0th i.e (first) and
-                    // (size-1)th i.e (last) element
-                    if (size1 % 2 == 1)
-                    {
-                        int temp = a1[0];
-                        a1[0] = a1[size1 - 1];
-                        a1[size1 - 1] = temp;
-                    }
-
-                    // If size is even, swap ith and
-                    // (size-1)th i.e (last) element
-                    else
-                    {
-                        int temp = a1[i];
-                        a1[i] = a1[size1 - 1];
-                        a1[size1 - 1] = temp;
-                    }
-                }
-
-            }
-        }
-
-        public static bool CheckGraphIsomorphism(Graph<T> g1, Graph<T> g2)
-        {
-            Console.WriteLine("STARTING GRAPH ISOMORPHISM CHECK");
-
-            //first look at the order and size
-            if (g1.TotalVertices != g2.TotalVertices || g1.TotalEdges != g2.TotalEdges)
-                return false;
-
-            int totalVertices = g1.TotalVertices;
-
-            var g1_DegreeSeq = g1.GetDegreeSequence();
-            var g2_DegreeSeq = g2.GetDegreeSequence();
-
-
-            //check the degree sequence next
-            for (int i = 0; i < totalVertices; i++)
-                if (g1_DegreeSeq[i] != g2_DegreeSeq[i])
-                    return false;
-
-            var g1_adj = g1.GetAdjacencyMatrix();
-            var g2_adj = g2.GetAdjacencyMatrix();
-
-            //check adjacency matrix here
-            int adjEqualCount = 0;
-            for (int i = 0; i < totalVertices; i++)
-                for (int j = 0; j < totalVertices; j++)
-                    if (g1_adj[i, j] == g2_adj[i, j])
-                        adjEqualCount++;
-
-            if(adjEqualCount == totalVertices * totalVertices)
-            {
-                Console.WriteLine("Identical Adjacency Matrices");
-                return true;
-            }
-
-            //check eigenvalues here...
-
-
-            //check permutations here...
-
-            //map each possible degree in g1 to a list of vertices with that degree
-            var g1_degreeMap = new Dictionary<int, List<int>>();
-            var g2_degreeMap = new Dictionary<int, List<int>>();
-
-            for (int i = 0; i < totalVertices; i++)
-            {
-                var degree_g1 = g1.graphStructure[i].GetDegree();
-                if (g1_degreeMap.ContainsKey(degree_g1))
-                    g1_degreeMap[degree_g1].Add(i);
-                else
-                    g1_degreeMap.Add(degree_g1, new List<int>() { i });
-
-                var degree_g2 = g2.graphStructure[i].GetDegree();
-                if (g2_degreeMap.ContainsKey(degree_g2))
-                    g2_degreeMap[degree_g2].Add(i);
-                else
-                    g2_degreeMap.Add(degree_g2, new List<int>() { i });
-            }
-
-            //map vertices in g1 (keys) to exact matches in g2 for isomorphism (because they have a unique degree)
-            var mapG1VerticesToG2Vertices = new Dictionary<int, int>();
-
-
-            var permsFromG1ToG2 = new Dictionary<List<int>, List<int[]>>();
-
-            
-            foreach (var (degree, g1_node_list) in g1_degreeMap)
-            {
-                int totalVerticesOfDegree = g1_node_list.Count;
-                if (totalVerticesOfDegree == 1)
-                {
-                    var g1_singleVertex = g1_degreeMap[degree][0];
-                    var g2_singleVertex = g2_degreeMap[degree][0];
-
-                    //map these nodes
-                    mapG1VerticesToG2Vertices[g1_singleVertex] = g2_singleVertex;
-                    Console.WriteLine("Mapping g1 Vertex {0} to g2 Vertex {1} due to unique degree {2}", g1_singleVertex, g2_singleVertex, degree);
-                }
-                else
-                {
-                    var perms = new List<int[]>();
-
-                    heapPermutations(g2_degreeMap[degree].ToArray(), totalVerticesOfDegree, totalVerticesOfDegree, perms);
-
-                    permsFromG1ToG2.Add(g1_node_list, perms);
-                }
-
-            }
-
-            //need to pick a perm from each perm set...
-
-            var permIndexer = new Dictionary<int, List<int>>();
-
-            var permutationPicker = new List<int[]>();
-
-            int perms_processed = 0;
-            foreach(var key in permsFromG1ToG2.Keys)
-            {
-                var permList = permsFromG1ToG2[key];
-
-                permIndexer.Add(perms_processed, key);
-
-                //for the first perm set, simply add them all
-                if (perms_processed == 0)
-                {
-                    //add all possible perms to the permutation picker
-                    for (int select_perm = 0; select_perm < permList.Count; select_perm++)
-                        permutationPicker.Add(new int[] { select_perm });
-                }
-                else
-                {
-                    var clone_old_perms = permutationPicker.ToArray();
-                    //clear the old perms
-                    permutationPicker.Clear();
-
-                    for (int select_perm = 0; select_perm < permList.Count; select_perm++)
-                    {
-                        for(int prev_perm = 0; prev_perm < clone_old_perms.Length; prev_perm++)
-                        {
-                            var add_next_perm = new int[perms_processed + 1];
-                            Array.Copy(clone_old_perms[prev_perm], add_next_perm, perms_processed);
-                            add_next_perm[perms_processed] = select_perm;
-
-                            permutationPicker.Add(add_next_perm);
-                        }
-                    }
-                }
-                
-                perms_processed++;
-            }
-
-            //G2 = PG1P^(-1)
-
-            var G1 = new Matrix(g1_adj);
-            var G2 = new Matrix(g2_adj);
-
-            //create a template permutation matrix
-            var template_perm_matrix = new int[totalVertices, totalVertices];
-            //link up the unique vertices
-            foreach (var kvp in mapG1VerticesToG2Vertices)
-            {
-                int vertexInG1 = kvp.Key;
-                int vertexInG2 = kvp.Value;
-                template_perm_matrix[vertexInG2, vertexInG1] = 1;
-            }
-
-            //new Matrix(template_perm_matrix).Print();
-
-            foreach (int[] perm_pick in permutationPicker)
-            {
-                //create the permutation matrix for the vertices from G1 to vertices in G2
-                var P = new Matrix(template_perm_matrix);
-
-                //obtain the set value for each permutation
-                for (int perm_set = 0; perm_set < perm_pick.Length; perm_set++)
-                {
-                    var perm_index = perm_pick[perm_set];
-
-                    var perm_key = permIndexer[perm_set];
-
-                    var perm_value = permsFromG1ToG2[perm_key][perm_index];
-
-                    for (int i = 0; i < perm_key.Count; i++) {
-
-                        int vertexInG1 = perm_key[i];
-                        int vertexInG2 = perm_value[i];
-                        P[vertexInG2, vertexInG1] = 1;
-                    }
-                }
-
-                var result = P * G1 * P.GetInverseMatrix();
-
-                if (G2.Equals(result))
-                {
-                    Console.WriteLine("\nIsomorphic!");
-
-                    Console.WriteLine("G1");
-                    G1.Print();
-                    Console.WriteLine("G2");
-                    G2.Print();
-                    Console.WriteLine("PG1P^(-1)");
-                    result.Print();
-
-                    Console.WriteLine("P");
-                    P.Print();
-
-                    for (int i = 0; i < totalVertices; i++)
-                        for (int j = 0; j < totalVertices; j++)
-                            if(P[i,j] == 1)
-                                Console.WriteLine("Mapped g1 Vertex {0} to g2 Vertex {1}", i, j);
-
-                    return true;
-                }
-
-            } 
-
-            return false;
-        }
-
-
         public bool IsValidMinor(Graph<int> checkMinor)
         {
             if (checkMinor.TotalVertices > TotalVertices || checkMinor.TotalEdges > TotalEdges)
@@ -987,7 +660,6 @@ namespace PathfindingTutorial.Data_Structures
 
             //clone the minor
             var minorClone = checkMinor.Clone();
-
 
             //First, add as many vertices as needed...
             int vertexDifference = TotalVertices - minorClone.TotalVertices;
@@ -1126,7 +798,7 @@ namespace PathfindingTutorial.Data_Structures
         public void PrintAdjacencyMatrix(int[,] provided_adjMatrix = null)
         {
             var sb = new StringBuilder("   ");
-            var adjacencyMatrix = provided_adjMatrix == null ? GetAdjacencyMatrix() : provided_adjMatrix;
+            var adjacencyMatrix = provided_adjMatrix ?? GetAdjacencyMatrix();
 
             var totalVertices = adjacencyMatrix.GetLength(0);
 
--- PathfindingTutorial/MakeIsomorphism.cs
+++ PathfindingTutorial/MakeIsomorphism.cs
@@ -9,6 +9,40 @@ namespace PathfindingTutorial
         private static void IsIsomorphic()
         {
 
+            var a1 = new GraphNode<int>(1);
+            var b1 = new GraphNode<int>(2);
+            var c1 = new GraphNode<int>(3);
+            var d1 = new GraphNode<int>(4);
+            var e1 = new GraphNode<int>(5);
+            var f1 = new GraphNode<int>(6);
+
+            IGraphNode<int>.AddMutualNeighbor(a1, b1);
+            IGraphNode<int>.AddMutualNeighbor(b1, f1);
+            IGraphNode<int>.AddMutualNeighbor(b1, c1);
+            IGraphNode<int>.AddMutualNeighbor(c1, d1);
+            IGraphNode<int>.AddMutualNeighbor(d1, e1);
+
+
+            var G1 = new Graph<int>(a1, b1, c1, d1, e1, f1);
+
+            var a2 = new GraphNode<int>(1);
+            var b2 = new GraphNode<int>(2);
+            var c2 = new GraphNode<int>(3);
+            var d2 = new GraphNode<int>(4);
+            var e2 = new GraphNode<int>(5);
+            var f2 = new GraphNode<int>(6);
+
+            IGraphNode<int>.AddMutualNeighbor(a2, b2);
+            IGraphNode<int>.AddMutualNeighbor(b2, c2);
+            IGraphNode<int>.AddMutualNeighbor(b2, f2);
+            IGraphNode<int>.AddMutualNeighbor(c2, d2);
+            IGraphNode<int>.AddMutualNeighbor(d2, e2);
+
+            var G2 = new Graph<int>(f2, e2, d2, c2, b2, a2);
+
+            Graph<int>.CheckGraphIsomorphism(G1, G2);
+
+            /*
             var g1_adj = new int[4, 4];
             g1_adj[0, 1] = 1;
             g1_adj[1, 2] = 1;
@@ -46,7 +80,7 @@ namespace PathfindingTutorial
             var G4 = Graph<int>.GenerateGraphForAdjacencyMatrix(g4_adj, true);
 
             Console.WriteLine(Graph<int>.CheckGraphIsomorphism(G3, G4));
-
+            */
         }
     }
 }
